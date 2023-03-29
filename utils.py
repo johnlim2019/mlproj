caf74aff1ba85ff637216f7e8663544384032f1f
kang ming <kangmingpc@gmail.com>
2023-03-29 09:58:22 +0800

    added utils.py

---
+++ utils.py
@@ -0,0 +1,12 @@
+# generate train data
+def get_train_data(train_path):
+    X = []
+    y = []
+    with open(path, 'r') as file:
+        for line in file:
+            if(len(line.rstrip().split()) == 0):
+                pass
+            else:
+                X.append(line.rstrip().split()[0])
+                y.append(line.rstrip().split()[1])
+    return X, y
