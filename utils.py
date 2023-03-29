2465232e36f5e220873ec7f38b3655acd086bb73
johnlim2019 <69998939+johnlim2019@users.noreply.github.com>
2023-03-29 12:41:42 +0800

    new utils

--- utils.py
+++ utils.py
@@ -1,24 +1,23 @@
 # generate train data
 from pprint import pprint
 def get_train_data(train_path):
-    x = []
-    y = []
-    y_one = []
-    x_one = []
+    # split  text into sentences for both hidden and observable variables
+    # also find all the discrete hidden states
+    x = ['START']
+    y = ['START']
     with open(train_path, 'r') as file:
         lineCounter = 0 
-        for line in file.readlines()[:50]:
+        for line in file.readlines()[:100]:
             if(len(line.rstrip().split()) == 0):
                 if lineCounter > 0:
-                    y.append(y_one)
-                    x.append(x_one)
-                    y_one = []
-                    x_one = []
+                    y.append("STOP")
+                    y.append("START")
+                    x.append("STOP")
+                    x.append("START")
             else:
-                x_one.append(line.rstrip().split()[0])
-                y_one.append(line.rstrip().split()[1])
+                x.append(line.rstrip().split()[0])
+                y.append(line.rstrip().split()[1])
             lineCounter += 1
     return x, y
 
 
-x,y = get_train_data("EN/train")
